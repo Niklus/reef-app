23023c13e4ffe84badf43effc31e869c094c50d4
Nicholas Otieno <niklasoti@gmail.com>
2022-04-02 13:32:26 +0200

    typo fix

--- app/pages/about.js
+++ app/pages/about.js
@@ -4,7 +4,7 @@ export function about(data) {
   return /*html*/ `
     <h2>About Page</h2>
     <h3>${data.count}</h3>
-    <button onclick="increment()">Inrement</button>
+    <button onclick="increment()">Increment</button>
     <button onclick="decrement()">Decrement</button>
     <hr/>
     ${users(data)}
--- app/pages/home.js
+++ app/pages/home.js
@@ -4,7 +4,7 @@ export function home(data) {
   return /*html*/ `
     <h2>Home Page</h2>
     <h3>${data.count}</h3>
-    <button onclick="increment()">Inrement</button>
+    <button onclick="increment()">Increment</button>
     <button onclick="decrement()">Decrement</button>
     <hr/>
     ${users(data)}
